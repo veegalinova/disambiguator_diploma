0e17b2a04ed5dd6c1329fdc1a18e40aa33c66c3e
galinova <galinova@sports.ru>
2019-01-29 17:18:15 +0300

    reloaded parser

---
+++ disambiguator/db.py
@@ -0,0 +1,52 @@
+import sqlite3
+
+
+class RutezDB:
+    def __init__(self, db_file: str):
+        self.cursor = sqlite3.connect(db_file).cursor()
+
+    def select_words_db_ids(self, words):
+        query_params = ','.join(['\'' + word.upper() + '\'' for word in words])
+        query = 'SELECT entry, name, is_polysemic ' \
+                'FROM text_entry ' \
+                'WHERE name in ({})'
+        self.cursor.execute(query.format(query_params))
+        rows = self.cursor.fetchall()
+        result = {word.lower(): (idx, poly) for idx, word, poly in rows}
+        return result
+
+    def select_close_words(self, ids):
+        query_params = ','.join(ids)
+        query = 'SELECT id_from, concepts.name, entry_id, entry_id_to ' \
+                'FROM relations_from_meanings ' \
+                'INNER JOIN concepts on id_from = concepts.id ' \
+                'WHERE entry_id in ({})'
+        self.cursor.execute(query.format(query_params))
+        rows = self.cursor.fetchall()
+        result = {}
+        meanings = {}
+        for idx, meaning, entry_id, close_word in rows:
+            if entry_id not in result:
+                result[entry_id] = {close_word: idx}
+            else:
+                result[entry_id][close_word] = idx
+            if idx not in meanings:
+                meanings[idx] = meaning
+        return result, meanings
+
+    def select_entry_meanings(self, ids):
+        query_params = ','.join(ids)
+        query = 'SELECT id_from, concepts.name, entry_id ' \
+                'FROM relations_from_meanings ' \
+                'INNER JOIN concepts on id_from = concepts.id ' \
+                'WHERE entry_id in ({}) ' \
+                'ORDER BY entry_id '
+        self.cursor.execute(query.format(query_params))
+        rows = self.cursor.fetchall()
+        result = {}
+        for idx, meaning, entry_id in rows:
+            if entry_id not in result:
+                result[entry_id] = {idx: meaning}
+            else:
+                result[entry_id][idx] = meaning
+        return result
---
+++ disambiguator/parser.py
@@ -0,0 +1,126 @@
+import re
+import string
+
+import yaml
+import pymorphy2
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+from yargy.tokenizer import MorphTokenizer
+import pandas as pd
+
+from disambiguator.db import RutezDB
+
+with open("config.yml", 'r') as ymlfile:
+    config = yaml.load(ymlfile)
+
+
+# todo : add named entities extraction?
+class TextProcessor:
+    def __init__(self, database=None):
+        self.morph = pymorphy2.MorphAnalyzer()
+        self.stop_words = stopwords.words('russian') + [s for s in string.punctuation]
+        self.morph_tokenizer = MorphTokenizer()
+        self.db = RutezDB(database)
+
+    # todo: add sentence borders
+    def morph_tokenize(self, text, stopwords=False):
+        # text = re.sub('\W+|\d+', ' ', text).lower()
+        result = []
+        text = text.lower()
+        tokens = self.morph_tokenizer(text)
+        if stopwords:
+            [result.append(
+                {'orig': token.value, 'word': token.normalized, 'position': token.span})
+                for token in tokens if token.normalized not in self.stop_words]
+        else:
+            [result.append(
+                {'orig': token.value, 'word': token.normalized, 'position': token.span})
+                for token in tokens]
+        return result
+
+    @staticmethod
+    def extract_text_from_htm(file):
+        with open(file) as htm:
+            soup = BeautifulSoup(htm, 'html.parser')
+            [s.extract() for s in soup('nomorph')]
+            [s.extract() for s in soup('title')]
+            [s.append('.') for s in soup(re.compile('^h[1-6]$'))]
+            text = soup.get_text().replace('\n', ' ').lstrip()
+            return text
+
+    # todo: add better name
+    def return_dict_tokens(self, tokens):
+        query_items = [token['word'] for token in tokens if token not in self.stop_words]
+        query = self.db.select_words_db_ids(query_items)
+        for token in tokens:
+            res = query.get(token['word'])
+            if res:
+                entry_id, is_poly = res
+                token['entry_id'] = entry_id
+                token['is_poly'] = is_poly
+        return tokens
+
+    def find_relations(self, tokens):
+        query_items = [str(token['entry_id']) for token in tokens if token.get('is_poly') == 1]
+        query, meanings = self.db.select_close_words(query_items)
+
+        for un_tok in tokens:
+            if un_tok.get('is_poly') == 0:
+                for token in tokens:
+                    if token.get('is_poly') == 1:
+                        close_words = query.get(token['entry_id'])
+                        if close_words.get(un_tok['entry_id']):
+                            if 'suggest' not in un_tok:
+                                un_tok['suggest'] = [{token['word'] : meanings.get(close_words.get(un_tok['entry_id']))}]
+                            else:
+                                if {token['word'] : meanings.get(close_words.get(un_tok['entry_id']))} not in un_tok['suggest']:
+                                    un_tok['suggest'].append({token['word'] : meanings.get(close_words.get(un_tok['entry_id']))})
+
+                            if 'meaning' not in token:
+                                token['meaning'] = [{meanings.get(close_words.get(un_tok['entry_id'])):un_tok['word']}]
+                            else:
+                                if {meanings.get(close_words.get(un_tok['entry_id'])):un_tok['word']} not in token['meaning']:
+                                    token['meaning'].append({meanings.get(close_words.get(un_tok['entry_id'])):un_tok['word']})
+
+        return tokens
+
+
+def process_file(database, file):
+    processor = TextProcessor(database)
+    text = processor.extract_text_from_htm(file)
+    tokens = processor.morph_tokenize(text)
+    tokens = processor.return_dict_tokens(tokens)
+    return processor.find_relations(tokens)
+
+
+def process_df(database, file):
+    processor = TextProcessor(database)
+    df = pd.read_csv(file)
+    texts = df['text1'].values + '. ' + df['text2'].values
+    result = []
+    with open('res_file.txt', 'w') as file:
+        for text in texts:
+            tokens = processor.morph_tokenize(text)
+            tokens = processor.return_dict_tokens(tokens)
+            relations = processor.find_relations(tokens)
+            for word in relations:
+                if 'meaning' in word:
+                    result.append(str(word['orig']).upper())
+                if 'suggest' in word:
+                    result.append(str(word['orig']) + ' ' + str(word['suggest']))
+                else:
+                    result.append(word['orig'])
+                file.write(result[-1])
+                file.write(' ')
+            file.write('\n')
+
+    return result
+
+
+def main():
+    process_df(config['database'], config['text'])
+
+
+if __name__ == '__main__':
+    main()
+
